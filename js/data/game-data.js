9b87fff4cd06ba560696cdad902dfb7c6ce8d0b0
Maria Ivanova <ivanova.m241@gmail.com>
2018-11-28 11:14:22 +0300

    убирает магические значения и исправляет функцию подсчета очков

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -4,12 +4,24 @@ export const INITIAL_GAME = Object.freeze({
   time: 0
 });
 
-export const answerScore = {
+export const AnswerScore = {
   WRONG: 0,
   SLOW: 50,
   NORMAL: 100,
   FAST: 150
 };
+
+export const AnswerTime = {
+  SLOW: 30,
+  NORMAL: 20,
+  FAST: 10
+};
+
+export const LIFE_BONUS = 50;
+
+export const MAX_QUANTITY_LIVES = 3;
+
+export const MAX_QUANTITY_QUESTIONS = 10;
 
 export const createAnswer = (isCorrect, time) => {
   const answer = {
--- js/data/point-scoring.js
+++ js/data/point-scoring.js
@@ -1,26 +1,35 @@
-import {answerScore} from './game-data.js';
+import {AnswerScore, AnswerTime, LIFE_BONUS, MAX_QUANTITY_QUESTIONS} from './game-data.js';
+
+const definePointsForAnswer = (answer) => {
+  let answerPoints;
+  if (answer.time < AnswerTime.FAST) {
+    answerPoints = AnswerScore.FAST;
+  } else if (answer.time >= AnswerTime.FAST && answer.time < AnswerTime.NORMAL) {
+    answerPoints = AnswerScore.NORMAL;
+  } else if (answer.time > AnswerTime.NORMAL) {
+    answerPoints = AnswerScore.SLOW;
+  }
+  return answerPoints;
+};
 
 export const scorePoints = (answers, lives) => {
   if (typeof lives !== `number` || typeof answers !== `object`) {
     throw new Error(`Lives should be type of number and answers should be type of object`);
   }
+
   if (lives < 0) {
     throw new Error(`Lives should not be negative value`);
   }
   let points = 0;
-  const bonus = lives * 50;
+  const bonus = lives * LIFE_BONUS;
   const rightAnswers = answers.filter((answer) => answer.isCorrect);
-  rightAnswers.forEach((answer) => {
-    if (answer.time < 0) {
+
+  points = rightAnswers.reduce((sum, currentAnswer) => {
+    if (currentAnswer.time < 0) {
       throw new Error(`Time should not be negative value`);
-    } else if (answer.time < 10) {
-      points += answerScore.FAST;
-    } else if (answer.time >= 10 && answer.time < 20) {
-      points += answerScore.NORMAL;
-    } else if (answer.time > 20) {
-      points += answerScore.SLOW;
     }
-  });
+    return sum + definePointsForAnswer(currentAnswer);
+  }, 0);
   points += bonus;
-  return answers.length < 10 ? -1 : points;
+  return answers.length < MAX_QUANTITY_QUESTIONS ? -1 : points;
 };
