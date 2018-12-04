52f1a8b52b54816dc9174d2da5e7ff94f53170b4
Maria <36799416+Mashka241@users.noreply.github.com>
2018-12-04 22:49:05 +0300

    Добавляет шаблоны для второй и третьей игр

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -33,47 +33,66 @@ export const createAnswer = (isCorrect, time) => {
 
 export const game1 = {
   task: `Угадайте для каждого изображения фото или рисунок?`,
-  // imgQuantity: 2,
   imgData: [`https://k42.kn3.net/CF42609C8.jpg`, `http://i.imgur.com/1KegWPz.jpg`],
   gameContent() {
-    return `<form class="game__content">
-      <div class="game__option">
-      <img src="${this.imgData[0]}" alt="Option 1" width="468" height="458">
+    const content = `<form class="game__content">
+    ${this.imgData.map((img, index) => {
+      return `<div class="game__option">
+      <img src="${img}" alt="Option 1" width="468" height="458">
       <label class="game__answer game__answer--photo">
-        <input class="visually-hidden" name="question1" type="radio" value="photo">
+        <input class="visually-hidden" name="question${index + 1}" type="radio" value="photo">
         <span>Фото</span>
       </label>
       <label class="game__answer game__answer--paint">
-        <input class="visually-hidden" name="question1" type="radio" value="paint">
+        <input class="visually-hidden" name="question${index + 1}" type="radio" value="paint">
         <span>Рисунок</span>
       </label>
-    </div>
-    <div class="game__option">
-      <img src="${this.imgData[1]}" alt="Option 2" width="468" height="458">
-      <label class="game__answer  game__answer--photo">
-        <input class="visually-hidden" name="question2" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer  game__answer--paint">x§
-        <input class="visually-hidden" name="question2" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>
-    </div>
+    </div>`
+    })}
     </form>`;
+    return content;
   },
   stats: [`wrong`, `slow`, `fast`, `correct`, ...new Array(6).fill(`unknown`)]
 };
 
-// const game2 = {
-//   task: `Угадай, фото или рисунок?`,
-//   imgQuantity: 1,
-//   imgData: [`https://k42.kn3.net/D2F0370D6.jpg`],
-//   stats: [`wrong`, `slow`, `fast`, `correct`, `wrong`, `unknown`, `slow`, `unknown`, `fast`, `unknown`]
-// };
+export const game2 = {
+  task: `Угадай, фото или рисунок?`,
+  imgData: [`https://k42.kn3.net/D2F0370D6.jpg`],
+  gameContent() {
+    const content = `<form class="game__content  game__content--wide">
+    <div class="game__option">
+      <img src="${this.imgData[0]}" alt="Option 1" width="705" height="455">
+      <label class="game__answer  game__answer--photo">
+        <input class="visually-hidden" name="question1" type="radio" value="photo">
+        <span>Фото</span>
+      </label>
+      <label class="game__answer  game__answer--paint">
+        <input class="visually-hidden" name="question1" type="radio" value="paint">
+        <span>Рисунок</span>
+      </label>
+    </div>
+  </form>`;
+  return content;
+  },
+  stats: [`wrong`, `slow`, `fast`, `correct`, `wrong`, `unknown`, `slow`, `unknown`, `fast`, `unknown`]
+};
 
-// export const game3 = {
-//   task: `Найдите рисунок среди изображений`,
-//   imgQuantity: 3,
-//   imgData: [`https://k32.kn3.net/5C7060EC5.jpg`, `https://i.imgur.com/DiHM5Zb.jpg`, `http://i.imgur.com/DKR1HtB.jpg`],
-//   stats: [`wrong`, `slow`, `fast`, `correct`, `wrong`, `unknown`, `slow`, `unknown`, `fast`, `unknown`]
-// };
+export const game3 = {
+  task: `Найдите рисунок среди изображений`,
+  imgData: [`https://k32.kn3.net/5C7060EC5.jpg`, `https://i.imgur.com/DiHM5Zb.jpg`, `http://i.imgur.com/DKR1HtB.jpg`],
+  gameContent() {
+    const content = `<form class="game__content  game__content--triple">
+    <div class="game__option">
+      <img src="${this.imgData[0]}" alt="Option 1" width="304" height="455">
+    </div>
+    <div class="game__option  game__option--selected">
+      <img src="${this.imgData[1]}" alt="Option 2" width="304" height="455">
+    </div>
+    <div class="game__option">
+      <img src="${this.imgData[2]}" alt="Option 3" width="304" height="455">
+    </div>
+  </form>`;
+    return content;
+  },
+  stats: [`wrong`, `slow`, `fast`, `correct`, `wrong`, `unknown`, `slow`, `unknown`, `fast`, `unknown`]
+};
