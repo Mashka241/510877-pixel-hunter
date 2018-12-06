258498590836ac91fd1da04f5baf585c5131e341
Maria <36799416+Mashka241@users.noreply.github.com>
2018-12-06 22:18:10 +0300

    убирает ошибки трэвис

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -37,7 +37,7 @@ export const game1 = {
   gameContent() {
     const content = `<form class="game__content">
     ${this.imgData.map((img, index) => {
-      return `<div class="game__option">
+    return `<div class="game__option">
       <img src="${img}" alt="Option 1" width="468" height="458">
       <label class="game__answer game__answer--photo">
         <input class="visually-hidden" name="question${index + 1}" type="radio" value="photo">
@@ -47,8 +47,8 @@ export const game1 = {
         <input class="visually-hidden" name="question${index + 1}" type="radio" value="paint">
         <span>Рисунок</span>
       </label>
-    </div>`
-    })}
+  </div>`;
+  })}
     </form>`;
     return content;
   },
@@ -71,8 +71,8 @@ export const game2 = {
         <span>Рисунок</span>
       </label>
     </div>
-  </form>`;
-  return content;
+</form>`;
+    return content;
   },
   stats: [`wrong`, `slow`, `fast`, `correct`, `wrong`, `unknown`, `slow`, `unknown`, `fast`, `unknown`]
 };
